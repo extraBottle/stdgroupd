ec11bb146a79a298c204176d8e1e44b505e27c06
extraBottle <tim2425@naver.com>
2024-12-18 06:11:13 +0900

    add: profile

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -16,9 +16,87 @@
         </template>
         <template v-slot:append>
           <q-btn color="primary" text-color="black" icon="person" round >
-            <q-menu>
-              <div>              
-                <q-img src="profileExample.png" style="width: 150px; height: 200px;"/>              
+            <q-menu ref="menuRef" @hide="beforeHoney = true">
+              <div v-if="beforeHoney" class="column no-wrap q-pb-md q-ml-md" style="min-width: 300px;">
+                <div class="row justify-end"><q-btn icon="close" @click="closeMenu" flat /></div>   
+                <div class="row items-center q-pl-md q-pb-md">
+                  <q-btn color="secondary" text-color="primary" icon="person" round unelevated />
+                  <div class="text-h6 text-weight-bold q-pl-md" >Hello, Sue Kim</div>
+                </div>                                                               
+                <div class="text-subtitle1 text-weight-bold q-pl-md q-pb-md" >My Coupons</div>
+                <Carousel v-bind="config">
+                  <Slide>
+                    <q-card style="border-radius: 15px;" class="q-mb-xs bg-primary">
+                      <div class="text-white q-pt-md">PETER PAN</div>
+                      <q-card-section class="row items-center no-wrap q-gutter-md">
+                        <div class="text-h4 text-weight-bold text-white">2,000<span class="text-h6">won</span></div>
+                        <q-btn icon="download" color="white" text-color="primary" round />
+                      </q-card-section>
+                      <div class="text-white q-pb-md">USE BY: 2024/12/31</div>
+                    </q-card>
+                  </Slide>
+                  <Slide>
+                    <q-card style="border-radius: 15px;" class="q-mb-xs bg-primary">
+                      <div class="text-white q-pt-md">YONSEI COOP</div>
+                      <q-card-section class="row items-center no-wrap q-gutter-md">
+                        <div class="text-h4 text-weight-bold text-white">1,000<span class="text-h6">won</span></div>
+                        <q-btn icon="download" color="white" text-color="primary" round />
+                      </q-card-section>
+                      <div class="text-white q-pb-md">USE BY: 2024/12/31</div>
+                    </q-card>
+                  </Slide>
+                </Carousel>
+                <div class="text-subtitle1 text-weight-bold q-pl-md q-py-md" >Achievements</div>
+                <Carousel v-bind="config2">
+                  <Slide>
+                    <q-card style="border-radius: 15px; width: 160px; height: 142px;" class="q-mb-xs bg-secondary" @click="beforeHoney = false">                      
+                      <q-card-section>
+                        <img src="profile1.png" />
+                        <div class="text-primary text-weight-bold">HoneyComb Process</div>
+                      </q-card-section>                                   
+                    </q-card>
+                  </Slide>
+                  <Slide>
+                    <q-card style="border-radius: 15px; width: 160px; height: 142px;" class="q-mb-xs bg-primary">                      
+                      <q-card-section>
+                        <img src="profile2.png" />
+                        <div class="text-secondary text-weight-bold">Badges</div>
+                      </q-card-section>                        
+                                        
+                    </q-card>
+                  </Slide>
+                  <Slide>
+                    <q-card style="border-radius: 15px; width: 160px; height: 142px;" class="q-mb-xs bg-secondary">                      
+                      <q-card-section>
+                        <img class="q-my-md" src="profile3.png" />
+                        <div class="text-primary text-weight-bold">Achievements</div>
+                      </q-card-section>
+                    </q-card>
+                  </Slide>
+                </Carousel>
+              </div>
+              <div v-else class="column no-wrap q-pb-md q-ml-lg" style="min-width: 300px;">
+                <div class="row justify-end"><q-btn icon="close" @click="closeMenu" flat /></div>   
+                <div class="text-h6 text-weight-bold q-pl-md q-mb-md" >My Honey Comb</div>
+                <q-card class="q-mr-lg relative-position" style="border-radius: 15px;">
+                  <q-img src="profile4.png" fit="scale-down" />
+                  <q-card-section class="bg-primary text-white text-weight-bold text-subtitle1">
+                    Complete Daily Mission
+                  </q-card-section>
+                  <q-chip class="absolute-bottom-left q-mb-xl" color="secondary" text-color="primary" >
+                    7/15 completed
+                  </q-chip>
+                </q-card>
+                <q-card class="q-mt-lg q-mr-lg bg-secondary relative-position" style="border-radius: 15px;">
+                  <q-img src="profile4.png" fit="scale-down" class="invisible" />
+                  <q-card-section class="bg-primary text-white text-weight-bold text-subtitle1 invisible">
+                    Complete Daily Mission
+                  </q-card-section>
+                  <div class="column items-center absolute-center">
+                    <q-icon class="q-mr-md" name="lock" color="primary" size="xl" />
+                    <div class="text-primary">Complete your hive to unlock this stage!</div>
+                  </div>
+                </q-card>
               </div>
             </q-menu>
           </q-btn>        
@@ -65,6 +143,8 @@
 <script setup>
 import { ref } from 'vue'
 import { useRoute } from 'vue-router';
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Slide } from 'vue3-carousel'
 import EssentialLink from 'components/EssentialLink.vue'
 import routes from 'src/router/routes'
 
@@ -75,7 +155,20 @@ const searchBar = ref()
 const route = useRoute()
 
 const selectCategory = ref('All')
+const menuRef = ref(null)
+const beforeHoney = ref(true)
 
+const config = {
+  itemsToShow: 1.5,
+  gap: 8
+};
+const config2 = {
+  itemsToShow: 1.8
+};
+
+function closeMenu(){
+  menuRef.value.hide()
+}
 function toggleLeftDrawer () {
   leftDrawerOpen.value = !leftDrawerOpen.value
 }
